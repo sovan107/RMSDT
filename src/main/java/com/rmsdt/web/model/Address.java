fa5859a5fdc08aef8c7b17519ff6d1f94069d368
sambitchoudhury <sambitgudu@yahoo.co.in>
2015-03-20 17:29:29 +0530

    Two new property lat and lng

--- src/main/java/com/rmsdt/web/model/Address.java
+++ src/main/java/com/rmsdt/web/model/Address.java
@@ -7,7 +7,6 @@ import javax.persistence.FetchType;
 import javax.persistence.JoinColumn;
 import javax.persistence.JoinTable;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.persistence.Transient;
 
@@ -16,34 +15,40 @@ import org.hibernate.validator.constraints.NotEmpty;
 @Entity
 @Table(name = "address")
 public class Address extends BaseEntity {
-	
-	@ManyToOne(cascade = { CascadeType.REFRESH }, fetch=FetchType.EAGER)
+
+	@ManyToOne(cascade = { CascadeType.REFRESH }, fetch = FetchType.EAGER)
 	@JoinTable(name = "events_address", joinColumns = @JoinColumn(name = "address_id_fk"), inverseJoinColumns = @JoinColumn(name = "event_id_fk"))
 	private Events event;
 
 	@Column(name = "house_no")
-	@NotEmpty(message="{validation.houseNo.required}")
+	@NotEmpty(message = "{validation.houseNo.required}")
 	private String houseNumber;
 
 	@Column(name = "street")
-	@NotEmpty(message="{validation.street.required}")
+	@NotEmpty(message = "{validation.street.required}")
 	private String street;
 
 	@Column(name = "city")
-	@NotEmpty(message="{validation.city.required}")
+	@NotEmpty(message = "{validation.city.required}")
 	private String city;
 
 	@Column(name = "state")
-	@NotEmpty(message="{validation.state.required}")
+	@NotEmpty(message = "{validation.state.required}")
 	private String state;
 
 	@Column(name = "country")
-	@NotEmpty(message="{validation.country.required}")
+	@NotEmpty(message = "{validation.country.required}")
 	private String country;
 
 	@Column(name = "postal_code")
-	@NotEmpty(message="{validation.postalCode.required}")
+	@NotEmpty(message = "{validation.postalCode.required}")
 	private String postalCode;
+
+	@Column(name = "lat")
+	private String lat;
+
+	@Column(name = "lng")
+	private String lng;
 
 	public String getHouseNumber() {
 		return houseNumber;
@@ -100,17 +105,31 @@ public class Address extends BaseEntity {
 	public void setEvent(Events event) {
 		this.event = event;
 	}
-	
-	@Transient
-	int ajaxId;
 
-	public int getAjaxId() {
+	@Transient
+	Integer ajaxId;
+
+	public Integer getAjaxId() {
 		return ajaxId;
 	}
 
-	public void setAjaxId(int ajaxId) {
+	public void setAjaxId(Integer ajaxId) {
 		this.ajaxId = ajaxId;
 	}
-	
-	
+
+	public String getLat() {
+		return lat;
+	}
+
+	public void setLat(String lat) {
+		this.lat = lat;
+	}
+
+	public String getLng() {
+		return lng;
+	}
+
+	public void setLng(String lng) {
+		this.lng = lng;
+	}
 }
